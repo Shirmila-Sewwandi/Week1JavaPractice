5bde2e27cd36ff6788b63c12a60e58c5b6f2faeb
Shirmila Sewwandi <105427132+Shirmila-Sewwandi@users.noreply.github.com>
2026-03-05 16:29:15 +1100

    Day 3 – Operators_EvenOddChecker

---
+++ src/EvenOddChecker.java
@@ -0,0 +1,11 @@
+public class EvenOddChecker {
+    public static void main(String[] args) {
+        int num1 = 12;
+
+        boolean isEvan = num1 % 2 == 0;
+
+        System.out.println("Number is: " + num1);
+        System.out.println("Is Evan: " + isEvan);
+
+        }
+}
